327c9c9aaedcec3d769ed67a37658cdeda5f0500
KanagawaYamada <albert.wesley.dion@gmail.com>
2026-06-01 21:39:15 +0700

    [Antigravity] This repo is messed but at least it worked
    Signed-off-by: KanagawaYamada <albert.wesley.dion@gmail.com>

---
+++ NewControlCenter/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (sudo ./install.sh)"
+  exit 1
+fi
+
+echo "Copying udev rules..."
+cp 99-byd-keyboard.rules /etc/udev/rules.d/
+udevadm control --reload-rules
+udevadm trigger
+
+echo "Setting up acpi_call permissions service..."
+cp acpi-call-perms.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable --now acpi-call-perms.service
+
+echo "Done! The Control Center should now work without sudo."
